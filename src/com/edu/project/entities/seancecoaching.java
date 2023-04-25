abfb5d0f792b9689ba69d0bcd954b962c870ce80
nada17 <nada.kort@esprit.tn>
2023-04-25 12:48:17 +0100

    API Excel+Filtage

---
+++ src/com/edu/project/entities/seancecoaching.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.edu.project.entities;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author pc
+ */
+public class seancecoaching {
+    private Integer id;
+    private Date date_debut_seance , date_fin_seance ;
+    private Double prix_seance ;
+    private String description_seance ,image_seance , titre_seance ; 
+
+    public seancecoaching() {
+    }
+
+    public seancecoaching(Integer id, Date date_debut_seance, Date date_fin_seance, Double prix_seance, String description_seance, String image_seance, String titre_seance) {
+        this.id = id;
+        this.date_debut_seance = date_debut_seance;
+        this.date_fin_seance = date_fin_seance;
+        this.prix_seance = prix_seance;
+        this.description_seance = description_seance;
+        this.image_seance = image_seance;
+        this.titre_seance = titre_seance;
+    }
+
+    public seancecoaching(Date date_debut_seance, Date date_fin_seance, Double prix_seance, String description_seance, String image_seance, String titre_seance) {
+        this.date_debut_seance = date_debut_seance;
+        this.date_fin_seance = date_fin_seance;
+        this.prix_seance = prix_seance;
+        this.description_seance = description_seance;
+        this.image_seance = image_seance;
+        this.titre_seance = titre_seance;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDate_debut_seance() {
+        return date_debut_seance;
+    }
+
+    public void setDate_debut_seance(Date date_debut_seance) {
+        this.date_debut_seance = date_debut_seance;
+    }
+
+    public Date getDate_fin_seance() {
+        return date_fin_seance;
+    }
+
+    public void setDate_fin_seance(Date date_fin_seance) {
+        this.date_fin_seance = date_fin_seance;
+    }
+
+    public Double getPrix_seance() {
+        return prix_seance;
+    }
+
+    public void setPrix_seance(Double prix_seance) {
+        this.prix_seance = prix_seance;
+    }
+
+    public String getDescription_seance() {
+        return description_seance;
+    }
+
+    public void setDescription_seance(String description_seance) {
+        this.description_seance = description_seance;
+    }
+
+    public String getImage_seance() {
+        return image_seance;
+    }
+
+    public void setImage_seance(String image_seance) {
+        this.image_seance = image_seance;
+    }
+
+    public String getTitre_seance() {
+        return titre_seance;
+    }
+
+    public void setTitre_seance(String titre_seance) {
+        this.titre_seance = titre_seance;
+    }
+
+    @Override
+    public String toString() {
+        return "user{" + "id=" + id + ", date_debut_seance=" + date_debut_seance + ", date_fin_seance=" + date_fin_seance + ", prix_seance=" + prix_seance + ", description_seance=" + description_seance + ", image_seance=" + image_seance + ", titre_seance=" + titre_seance + '}';
+    }
+    
+    
+
+ 
+}
---
+++ src/pidev/seancecoachingmain.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pidev;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author ScaareCr0w
+ */
+public class seancecoachingmain extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+         Parent root =FXMLLoader.load(getClass().getResource("../com/edu/project/sidebar/Administration.fxml"));
+           //Parent root =FXMLLoader.load(getClass().getResource("../gui/frontOffice/home/home.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Administration");
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
